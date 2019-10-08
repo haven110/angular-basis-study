232a0d613d0185379e5e4e0b6a730fcd864a82b3
haveny <haveny@laxtongroup.com>
2019-10-08 20:00:03 +0800

    new

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -11,35 +11,35 @@ export class DataService{
 	}
 
 	getSingleUser(id){
-		return this.http.get("http://jsonplaceholder.typicode.com/users/"+id)
+		return this.http.get("https://jsonplaceholder.typicode.com/users/"+id)
 				   .map(res => res.json());
 	}
 
 	getUsers(){
-		return this.http.get("http://jsonplaceholder.typicode.com/users")
+		return this.http.get("https://jsonplaceholder.typicode.com/users")
 				   .map(res => res.json());
 	}
 
 	addUser(user:User){
-		return this.http.post("http://jsonplaceholder.typicode.com/users",user)
+		return this.http.post("https://jsonplaceholder.typicode.com/users",user)
 				   .map(res => res.json());
 	}
 
 	deleteUser(id){
-		return this.http.delete("http://jsonplaceholder.typicode.com/users/"+id)
+		return this.http.delete("https://jsonplaceholder.typicode.com/users/"+id)
 				   .map(res => res.json());
 	}
 
 	// updateUser(user){
-	// 	return this.http.put("http://jsonplaceholder.typicode.com/users/"+user.id,user)
+	// 	return this.http.put("https://jsonplaceholder.typicode.com/users/"+user.id,user)
 	// 			   .map(res => res.json());
 	// }
 	// updateUser(user){
-	// 	return this.http.post("http://jsonplaceholder.typicode.com/users",user)
+	// 	return this.http.post("https://jsonplaceholder.typicode.com/users",user)
 	// 	.map(res=>res.json());
 	// }
 	updateUser(user){
-		return this.http.post("http://jsonplaceholder.typicode.com/users",user)
+		return this.http.post("https://jsonplaceholder.typicode.com/users",user)
 		.map(res=>res.json());
 	}
 }
